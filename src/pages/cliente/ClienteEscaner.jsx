e16d66f127de4867867f899c96422eda882d29d7
Sebas <alejoguamialama@gmail.com>
2026-01-05 17:29:59 -0500

    src/cliente/escaner => src/scanner

--- src/pages/cliente/ClienteEscaner.jsx
+++ src/pages/cliente/ClienteEscaner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ClienteLayout from "../../cliente/layout/ClienteLayout";
-import EscanerView from "../../cliente/escaner/EscanerView";
+import EscanerView from "../../scanner/EscanerView";
 
 export default function ClienteEscaner() {
   return (
