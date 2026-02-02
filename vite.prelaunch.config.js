300779a4cee6d697fc4044da5762b404e5e8e220
Sebas <alejoguamialama@gmail.com>
2026-02-02 08:53:37 -0500

    not reading .env, changing directory on vite.prelaunch.config.js

--- vite.prelaunch.config.js
+++ vite.prelaunch.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: "prelaunch",
+  envDir: __dirname,
   publicDir: "../public",
   plugins: [react(), tailwindcss()],
   build: {
