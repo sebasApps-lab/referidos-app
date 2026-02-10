332953b330acd7e69faf8a58fd32306502e4fa91
Sebas <alejoguamialama@gmail.com>
2026-02-10 10:43:39 -0500

    chore(android): remove tracked .cxx build artifacts

--- apps/referidos-android/android/app/.cxx/Debug/1f4c186j/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Sebas/.gradle/caches/8.13/transforms/ecc360991f6e72abc8ac0f2009a2f64d/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.arm64-v8a/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Sebas/.gradle/caches/8.13/transforms/ecc360991f6e72abc8ac0f2009a2f64d/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- apps/referidos-android/android/app/.cxx/Debug/1f4c186j/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Sebas/.gradle/caches/8.13/transforms/c84dba0842784e07d95045a8302cd32b/transformed/hermes-android-0.79.7-debug/prefab/modules/libhermes/libs/android.arm64-v8a/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Sebas/.gradle/caches/8.13/transforms/c84dba0842784e07d95045a8302cd32b/transformed/hermes-android-0.79.7-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
